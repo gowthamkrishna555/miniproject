24f29c779e162d985cecfbb381867e6e20e35680
gowthamkrishna555 <117144261+gowthamkrishna555@users.noreply.github.com>
2023-01-15 19:01:02 +0530

    Add files via upload

---
+++ productdisplay.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+<?php
+    include('conn.php');
+
+//MySQL query goes here
+$sql = "SELECT  farmer.F_CONTACT,product.P_ID,product.P_NAME,product.P_TYPE,product.P_QTY,product.P_PRICE
+FROM farmer,product,sell
+WHERE sell.P_ID=product.P_ID AND farmer.F_ID=sell.F_ID";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table>
+    <tr>
+    <th>P_ID</th> <!This is HTML table heading>
+    <th>P_NAME</th>
+    <th>P_TYPE</th>
+    <th>P_QTY</th>
+    <th>P_PRICE</th>
+    <th>F_CONTACT</th>
+    </tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+        <td>" . $row["P_ID"]. "</td> <!This is HTML table data>
+        <td>" . $row["P_NAME"]. "</td> 
+        <td>" . $row["P_TYPE"]. "</td> 
+        <td>" . $row["P_QTY"]. "</td> 
+        <td>" . $row["P_PRICE"]. "</td> 
+        <td>" . $row["F_CONTACT"]. "</td> 
+        </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?> 
+
+</body>
+</html>
